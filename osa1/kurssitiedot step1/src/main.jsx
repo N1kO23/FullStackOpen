4a9dfb9be4ed71dfbffd2f857ca7c878ac434d4e
Niko <niko.huuskonen.00@gmail.com>
2024-03-25 02:41:35 +0200

    osa1 step2 tehty

---
+++ osa1/kurssitiedot step1/src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from "../../kurssitiedot step2/node_modules/@types/react-dom/client";
+
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
